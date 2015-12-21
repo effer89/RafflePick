c499c3dfe079bbc1c9074bf5dfefd8569e65ff45
Fernando Fernandes <effer89@gmail.com>
2015-12-21 18:04:35 -0200

    Update raffle.js

--- raffle.js
+++ raffle.js
@@ -1,58 +1,79 @@
-var host = 'http://csgorage.com';
-var curUrl = window.location.pathname;
+var url   = window.location.hostname,
+	slash = window.location.pathname,
+	botAlert = '<div class="effer_bots" style="background-color: red;text-align: center;position: fixed; width: 100%; top:0px;z-index:9999;">BOT ON!1!!!</div>';
 
-function getSlots() {
-	var slotsH = $('div.row#slots');
-	var slotsAvail = slotsH.children('div.slots');
-	var button = $('button.getbutton');
-	if(!button.is(':visible')){
-	    window.location = host+'/free-raffles';
-	}
-	if(slotsAvail.length > 0){
-
-		var max = 0,
-			min = slotsAvail.length,
-			selectedSlot = Math.floor(Math.random() * (max - min) + min);
-
-		//var slot = slotsAvail[0];
-		var slot = slotsAvail[selectedSlot];
-		$(slot).trigger('click');
-
-		if(button.is(':visible')){
-			button.trigger('click');
-		}
-
-		setTimeout(function(){
-			window.location = host+'/free-raffles';
-		},5000);
-	}else{
-		window.scrollTo(0,document.body.scrollHeight);
-		setTimeout(getSlots, 1000);
-	}
-}
-
-if(curUrl == '/'){
-	window.location = host+'/free-raffles';
-}
-
-if(curUrl == '/free-raffles'){
-	var riffle = $('body.pg_free-raffles div.container div.row div.main div.row:not(.category,.past_raffle)').slice(2);
-	riffle.each(function(index, value){
-		var row = $(value);
-		console.log(row.find('a div.ribbon').length);
-		if(row.find('a div.ribbon').length === 0){
-			var href = row.find('a').attr('href');
-			if(href !== undefined){
-				window.location = host+href;
+// csgorage.com
+if(url == 'csgorage.com'){
+	if(slash == '/free-raffles'){
+		$('body').append(botAlert);
+		var raffles = $('body.pg_free-raffles div.container div.row div.main div.row:not(.category,.past_raffle)').slice(2);
+		raffles.each(function(index, value){
+			var row = $(value);
+			if(row.find('a div.ribbon').length === 0){
+				var href = row.find('a').attr('href');
+				if(href !== undefined){
+					window.location = 'http://'+url+href;
+				}
 			}
+		});
+		setTimeout(function(){
+			window.location = 'http://'+url+'/free-raffles';
+		},30000);
+	}
+	if(slash.indexOf('raffles') === -1 && slash.indexOf('raffle') === 1){
+		getSlots();
+	}
+	function getSlots() {
+		var slotsH = $('div.row#slots');
+		var slotsAvail = slotsH.children('div.slots');
+		var button = $('button.getbutton');
+		if(!button.is(':visible')){
+		    window.location = 'http://'+url+'/free-raffles';
 		}
-	});
+		if(slotsAvail.length > 0){
+
+			var max = 0,
+				min = slotsAvail.length,
+				selectedSlot = Math.floor(Math.random() * (max - min) + min);
+
+			//var slot = slotsAvail[0];
+			var slot = slotsAvail[selectedSlot];
+			$(slot).trigger('click');
+
+			if(button.is(':visible')){
+				button.trigger('click');
+			}
+
+			setTimeout(function(){
+				window.location = 'http://'+url+'/free-raffles';
+			},5000);
+		}else{
+			window.scrollTo(0,document.body.scrollHeight);
+			setTimeout(getSlots, 1000);
+		}
+	}
 }
 
-if(curUrl.indexOf('raffle') === 1){
-	getSlots();
+// csgoprizes.com
+if(url == 'csgoprizes.com'){
+	if(slash == '/'){
+		//window.location = 'http://'+url+'/get-tickets';
+	}
+	if(slash == '/get-tickets'){
+		// when site is down
+		if(document.getElementById('compt') === null){
+			var frame = document.getElementsByTagName('iframe')[0];
+				frame.parentNode.removeChild(frame);
+			setTimeout(function(){
+				window.location = 'http://'+url+'/get-tickets';
+			}, 60000);
+		}
+		$('body').append(botAlert);
+		$('iframe').eq(0)[0].remove();
+		setInterval(function(){
+			if($('p#compt span').html() == '0'){
+				$('div.bouton_get_tickets a').eq(0)[0].click();
+			}
+		},5000);
+	}
 }
-
-setTimeout(function(){
-	window.location = host+'/free-raffles';
-},30000);
